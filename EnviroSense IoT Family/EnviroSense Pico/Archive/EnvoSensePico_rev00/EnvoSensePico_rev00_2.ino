ed53fbfc6b689e9c83b558c0f04741eae2ddac5d
yaghini <yaghini@ualberta.ca>
2025-09-17 13:41:02 -0700

    added enviroment_pico to the work order

---
+++ EnviroSense IoT Family/EnviroSense Pico/Archive/EnvoSensePico_rev00/EnvoSensePico_rev00_2.ino
@@ -0,0 +1,372 @@
+//#include <UTFT.h>
+
+#include <ESP8266WiFi.h>
+#include <ESP8266mDNS.h>
+#include <WiFiUdp.h>
+#include <ArduinoOTA.h>
+#include <NTPClient.h>
+#include <Credentials.h>
+#include <Adafruit_GFX.h>    // Core graphics library
+#include <Adafruit_ST7735.h> // Hardware-specific library for ST7735
+#include <SPI.h>
+#include "bsec.h"
+#include "PubSubClient.h" // Connect and publish to the MQTT broker
+
+#ifndef STASSID
+#define STASSID "your-ssid"
+#define STAPSK  "your-password"
+#endif
+const char* ssid = mySSID;
+const char* password = myPASSWORD;
+
+// BME680 Helper functions declarations
+void checkIaqSensorStatus(void);
+void errLeds(void);
+// Create an object of the class Bsec
+Bsec iaqSensor;
+String BMEoutput;
+String accuracyState;
+String insideTemp;
+String insideH;
+String aqiText;
+String saqi;
+String eCO2;
+int aqIndex;
+unsigned long previousAirMillis = 10;
+const unsigned long airInterval = 1 * 60 * 1000;
+int i = 0;
+//int j = 0;
+
+//TFT LCD
+#define TFT_CS         D3
+#define TFT_RST        RX
+#define TFT_DC         D4
+#define TFT_MOSI D7  // Data out
+#define TFT_SCLK D5  // Clock out
+Adafruit_ST7735 tft = Adafruit_ST7735(TFT_CS, TFT_DC, TFT_MOSI, TFT_SCLK, TFT_RST);
+
+// Define NTP Client to get time
+const long utcOffsetInSeconds = -25200;
+WiFiUDP ntpUDP;
+NTPClient timeClient(ntpUDP, "pool.ntp.org", utcOffsetInSeconds);
+WiFiClient client;
+String timeToDisplay = "waiting to receive the time from server";
+
+// MQTT
+const char* mqtt_server = "192.168.1.92";  // IP of the MQTT broker
+const char* humidity_topic = "home/EnviroSensePico/humidity";
+const char* temperature_topic = "home/EnviroSensePico/temperature";
+const char* aqi_topic = "home/EnviroSensePico/aqi";
+const char* aqiS_topic = "home/EnviroSensePico/staticaqi";
+const char* eCO2_topic = "home/EnviroSensePico/equvalentCO2";
+const char* accuracy_topic = "home/EnviroSensePico/accuracy";
+//const char* mqtt_username = "cdavid"; // MQTT username
+//const char* mqtt_password = "cdavid"; // MQTT password
+const char* clientID = "client_EnviroSensePico"; // MQTT client ID
+PubSubClient mqttClient(mqtt_server, 1883, client);
+
+//Time interval Settings
+long interval = 30000;           // interval at which to send (milliseconds)
+long previousMillis = interval;        // will store last time data was sent
+long dispInterval = 7000;           // interval at which to change display (milliseconds)
+long previousDispMillis = interval;        // will store last time data was sent
+int state = 0;
+int displayState = 0;
+
+
+
+
+void setup() {
+
+  tft.initR(INITR_MINI160x80);  // Init ST7735S mini display
+
+  TextWriter("Enviro  Sense   Pico v1", ST77XX_GREEN, 3);
+  delay(1000);
+  TextWriter("        ALI     YAGHINI", ST77XX_BLUE, 3);
+  delay(1000);
+
+  TextWriter("             Wi-Fi ...", ST77XX_WHITE, 2);
+  delay(1000);
+  WiFi.begin(ssid, password);
+  while ( WiFi.status() != WL_CONNECTED ) {
+    delay ( 500 );
+    //Serial.print ( "." );
+  }
+
+  TextWriter("             Updating Time", ST77XX_WHITE, 2);
+  delay(1000);
+  timeClient.begin();
+
+
+  //BME680
+  TextWriter("             BME-680      Setup..", ST77XX_WHITE, 2);
+  delay(1000);
+  Wire.begin();
+  iaqSensor.begin(0x77, Wire);
+  checkIaqSensorStatus();
+
+  bsec_virtual_sensor_t sensorList[10] = {
+    BSEC_OUTPUT_RAW_TEMPERATURE,
+    BSEC_OUTPUT_RAW_PRESSURE,
+    BSEC_OUTPUT_RAW_HUMIDITY,
+    BSEC_OUTPUT_RAW_GAS,
+    BSEC_OUTPUT_IAQ,
+    BSEC_OUTPUT_STATIC_IAQ,
+    BSEC_OUTPUT_CO2_EQUIVALENT,
+    BSEC_OUTPUT_BREATH_VOC_EQUIVALENT,
+    BSEC_OUTPUT_SENSOR_HEAT_COMPENSATED_TEMPERATURE,
+    BSEC_OUTPUT_SENSOR_HEAT_COMPENSATED_HUMIDITY,
+  };
+
+  iaqSensor.updateSubscription(sensorList, 10, BSEC_SAMPLE_RATE_LP);
+  checkIaqSensorStatus();
+
+  ArduinoOTA.begin();
+
+}
+
+void loop() {
+  ArduinoOTA.handle();
+  StateMachine();
+}
+
+void StateMachine() {
+  unsigned long currentMillis = millis();
+  switch (state) {
+    case 0: {
+        if (currentMillis - previousMillis > interval) {
+          previousMillis = currentMillis;
+          UpdateTime();
+        }
+        state++;
+        break;
+      }
+    case 1: {
+        if (iaqSensor.run()) {
+          updateBME();
+        }
+        else {
+          checkIaqSensorStatus();
+        }
+        state++;
+        break;
+      }
+    case 2: {
+        if (currentMillis - previousDispMillis > dispInterval) {
+          previousDispMillis = currentMillis;
+          DisplayManager();
+        }
+        state = 0;
+        break;
+      }
+  }
+}
+
+void UpdateTime() {
+  timeToDisplay = GetTime();
+  //TextWriter(timeToDisplay.c_str(), ST77XX_WHITE, 2);
+  //UpdateDisplayText();
+}
+
+
+String GetTime() {
+  timeClient.update();
+  //  int hour = timeClient.getHours();
+  //  int minute =  timeClient.getMinutes();
+  //  String sep = ":";
+  //  String currentTime = hour + sep + minute;
+
+  String currentTime = timeClient.getFormattedTime();
+  currentTime.remove(5, 3);
+  return currentTime;
+}
+
+void updateBME() {
+  i++;
+  UpdateBMEText();
+  if (i >= 500) {
+    //DisplayManager();
+    SendMQTT();
+    i = 0;
+    // j = 0;
+  }
+}
+
+void UpdateBMEText() {
+  aqIndex = iaqSensor.iaq;
+  aqiText = String(aqIndex);
+  accuracyState = String(iaqSensor.iaqAccuracy);
+  insideTemp = String(iaqSensor.temperature);
+  insideH =  String(iaqSensor.humidity);
+  saqi =  String(iaqSensor.staticIaq);
+  eCO2 = String(iaqSensor.co2Equivalent);
+}
+
+void DisplayManager() {
+  AlertManager();
+  switch (displayState) {
+    case 0: {
+        DisplayTime();
+        displayState++;
+        break;
+      }
+    case 1: {
+        DisplayAqi();
+        displayState ++;
+        break;
+      }
+    case 2: {
+        DisplayTemp();
+        displayState = 0;
+        break;
+      }
+  }
+}
+
+void DisplayAqi() {
+  //tft.setCursor(0, 30);
+  tft.setCursor(1, 1);
+  tft.setTextColor(ST77XX_BLUE);
+  tft.setTextSize(2);
+  tft.println(eCO2 + "ppm");
+  tft.setTextColor(ST77XX_GREEN);
+  tft.setTextSize(4);
+  tft.println(aqiText);
+  tft.setTextColor(ST77XX_YELLOW);
+  tft.setTextSize(2);
+  tft.println(insideH + "%");
+}
+
+void DisplayTime() {
+  tft.setCursor(0, 30);
+  tft.setTextColor(ST77XX_WHITE);
+  tft.setTextSize(4);
+  tft.println(" " + timeToDisplay);
+  tft.setTextColor(ST77XX_MAGENTA);
+  tft.setTextSize(2);
+  tft.println(insideTemp + "c");
+}
+
+void DisplayTemp() {
+  tft.setCursor(0, 30);
+  tft.setTextColor(ST77XX_MAGENTA);
+  tft.setTextSize(4);
+  tft.println(insideTemp + "c");
+}
+
+void AlertManager() {
+  if (aqIndex <= 200) {
+    tft.fillScreen(ST77XX_BLACK);
+  } else {
+    tft.fillScreen(ST77XX_RED);
+  }
+}
+
+void SendMQTT() {
+  // Connect to MQTT Broker
+  // client.connect returns a boolean value to let us know if the connection was successful.
+  // If the connection is failing, make sure you are using the correct MQTT Username and Password (Setup Earlier in the Instructable)
+  if (mqttClient.connect(clientID)) {
+    SendMQTTMessages(humidity_topic, String(insideH));
+    SendMQTTMessages(aqi_topic, String(aqIndex));
+    SendMQTTMessages(accuracy_topic, String(accuracyState));
+    SendMQTTMessages(aqiS_topic, String(saqi));
+    //SendMQTTMessages(aqiS_topic, saqi));
+    SendMQTTMessages(eCO2_topic, String(eCO2));
+    SendMQTTMessages(temperature_topic, String(insideTemp));
+
+    mqttClient.disconnect();  // disconnect from the MQTT broker
+    //errLeds();
+  }
+  else {
+    //Serial.println("Connection to MQTT Broker failed...");
+  }
+}
+
+void SendMQTTMessages(const char* topic, String parameter) {
+  if (!mqttClient.publish(topic, parameter.c_str())) {
+
+    // Again, client.publish will return a boolean value depending on whether it succeeded or not.
+    // If the message failed to send, we will try again, as the connection may have broken.
+
+    //Serial.println("Temperature failed to send. Reconnecting to MQTT Broker and trying again");
+    mqttClient.connect(clientID);
+    delay(10); // This delay ensures that client.publish doesn't clash with the client.connect call
+    mqttClient.publish(topic, parameter.c_str());
+  }
+}
+
+
+void TextWriter(String text, uint16_t color, int size) {
+  tft.fillScreen(ST77XX_BLACK);
+  tft.invertDisplay(true);
+  tft.setRotation(1);
+  tft.setCursor(0, 0);
+  tft.setTextSize(size);
+  tft.setTextColor(color);
+  tft.setTextWrap(true);
+  tft.print(text);
+
+}
+
+void rotateText() {
+  for (uint8_t i = 0; i < 4; i++) {
+    tft.fillScreen(ST77XX_BLACK);
+    Serial.println(tft.getRotation(), DEC);
+
+    tft.setCursor(0, 30);
+    tft.setTextColor(ST77XX_RED);
+    tft.setTextSize(1);
+    tft.println("Hello World!");
+    tft.setTextColor(ST77XX_YELLOW);
+    tft.setTextSize(2);
+    tft.println("Hello World!");
+    tft.setTextColor(ST77XX_GREEN);
+    tft.setTextSize(3);
+    tft.println("Hello World!");
+    tft.setTextColor(ST77XX_BLUE);
+    tft.setTextSize(4);
+    tft.print(1234.567);
+    delay(1000);
+
+    tft.setRotation(tft.getRotation() + 1);
+  }
+}
+
+// BME680 Helper function definitions
+void checkIaqSensorStatus(void)
+{
+  if (iaqSensor.status != BSEC_OK) {
+    if (iaqSensor.status < BSEC_OK) {
+      //output = "BSEC error code : " + String(iaqSensor.status);
+      //Serial.println(output);
+      for (;;)
+        errLeds(); /* Halt in case of failure */
+    } else {
+      //output = "BSEC warning code : " + String(iaqSensor.status);
+      //Serial.println(output);
+    }
+  }
+
+  if (iaqSensor.bme680Status != BME680_OK) {
+    if (iaqSensor.bme680Status < BME680_OK) {
+      //output = "BME680 error code : " + String(iaqSensor.bme680Status);
+      //Serial.println(output);
+      for (;;)
+        errLeds(); /* Halt in case of failure */
+    } else {
+      //output = "BME680 warning code : " + String(iaqSensor.bme680Status);
+      //Serial.println(output);
+    }
+  }
+}
+
+//BME680 Reated
+void errLeds(void)
+{
+  pinMode(LED_BUILTIN, OUTPUT);
+  digitalWrite(LED_BUILTIN, HIGH);
+  delay(100);
+  digitalWrite(LED_BUILTIN, LOW);
+  delay(100);
+}
---
+++ EnviroSense IoT Family/EnviroSense Pico/Arduino Code/EnviroSense_Pico_rev00/EnviroSense_Pico_rev00.ino
@@ -0,0 +1,322 @@
+#include <Wire.h>
+#include <ESP8266WiFi.h>
+#include <PubSubClient.h>
+#include <Adafruit_GFX.h>
+#include <Adafruit_ST7735.h>
+#include <Adafruit_BME680.h>
+
+// -------------------- WiFi + MQTT --------------------
+#include "secrets.h"
+// const char* ssid = "";
+// const char* password = "";
+
+// const char* mqtt_server = "";
+// const int mqtt_port = 1883;
+// const char* mqtt_user = "";
+// const char* mqtt_pass = "";
+
+WiFiClient espClient;
+PubSubClient mqttClient(espClient);
+
+// -------------------- Display --------------------
+//TFT LCD
+#define TFT_CS         D3
+#define TFT_RST        RX
+#define TFT_DC         D4
+#define TFT_MOSI D7  // Data out
+#define TFT_SCLK D5  // Clock out
+Adafruit_ST7735 tft = Adafruit_ST7735(TFT_CS, TFT_DC, TFT_MOSI, TFT_SCLK, TFT_RST);
+
+// Last displayed values
+float lastTemperature = NAN;
+float lastHumidity = NAN;
+float lastGas = NAN;
+String lastAqiText = "";
+
+// Thresholds
+const float tempThreshold = 1.0;      // degrees Celsius
+const float humidityThreshold = 5.0;  // percent
+const float gasThreshold = 100000.0;      // ohms (or adjust based on scale)
+
+// -------------------- BME680 --------------------
+Adafruit_BME680 bme; // I2C
+
+// Global sensor values
+float temperature = NAN;
+float humidity = NAN;
+float pressure = NAN;
+float gas_resistance = NAN;
+
+float tempOffset = -6.55;
+
+// -------------------- Timers --------------------
+unsigned long lastDisplayUpdate = 0;
+unsigned long lastMqttUpdate = 0;
+const unsigned long displayInterval = 2000;
+const unsigned long mqttInterval = 10000;
+
+// -------------------- MQTT Discovery --------------------
+void publishDiscovery() {
+  // Temperature
+  mqttClient.publish("homeassistant/sensor/enviroSense_temperature/config",
+    R"({
+      "name": "Temperature",
+      "state_topic": "EnviroSense/temperature",
+      "unit_of_measurement": "°C",
+      "device_class": "temperature"
+    })", true);
+
+  // Humidity
+  mqttClient.publish("homeassistant/sensor/enviroSense_humidity/config",
+    R"({
+      "name": "Humidity",
+      "state_topic": "EnviroSense/humidity",
+      "unit_of_measurement": "%",
+      "device_class": "humidity"
+    })", true);
+
+  // Pressure
+  mqttClient.publish("homeassistant/sensor/enviroSense_pressure/config",
+    R"({
+      "name": "Pressure",
+      "state_topic": "EnviroSense/pressure",
+      "unit_of_measurement": "hPa",
+      "device_class": "pressure"
+    })", true);
+
+  // Gas
+  mqttClient.publish("homeassistant/sensor/enviroSense_gas/config",
+    R"({
+      "name": "Gas",
+      "state_topic": "EnviroSense/gas",
+      "unit_of_measurement": "Ohm"
+    })", true);
+
+  // AQI
+  mqttClient.publish("homeassistant/sensor/enviroSense_aqi/config",
+    R"({
+      "name": "AQI",
+      "state_topic": "EnviroSense/aqi"
+    })", true);
+}
+
+
+
+
+// -------------------- MQTT Publish --------------------
+void publishState() {
+  char payload[32];
+
+  // Temperature
+  snprintf(payload, sizeof(payload), "%.2f", temperature);
+  mqttClient.publish("EnviroSense/temperature", payload, true);
+
+  // Humidity
+  snprintf(payload, sizeof(payload), "%.2f", humidity);
+  mqttClient.publish("EnviroSense/humidity", payload, true);
+
+  // Pressure
+  snprintf(payload, sizeof(payload), "%.0f", pressure);
+  mqttClient.publish("EnviroSense/pressure", payload, true);
+
+  // Gas
+  snprintf(payload, sizeof(payload), "%.0f", gas_resistance);
+  mqttClient.publish("EnviroSense/gas", payload, true);
+
+  // AQI
+  String aqiText;
+  if (gas_resistance > 30000) aqiText = "Good";
+  else if (gas_resistance > 15000) aqiText = "Moderate";
+  else aqiText = "Poor";
+
+  mqttClient.publish("EnviroSense/aqi", aqiText.c_str(), true);
+}
+
+// -------------------- Debug Print Function --------------------
+void printSensorData() {
+  Serial.println(F("---- BME680 Sensor Readings ----"));
+  Serial.print(F("Temperature: "));
+  Serial.print(temperature, 2);
+  Serial.println(F(" °C"));
+
+  Serial.print(F("Humidity: "));
+  Serial.print(humidity, 2);
+  Serial.println(F(" %"));
+
+  Serial.print(F("Pressure: "));
+  Serial.print(pressure, 2);
+  Serial.println(F(" hPa"));
+
+  Serial.print(F("Gas Resistance: "));
+  Serial.print(gas_resistance, 0);
+  Serial.println(F(" Ohms"));
+
+  Serial.println(F("-------------------------------"));
+}
+
+// -------------------- BME680 Reading --------------------
+void readBME680() {
+  if (!bme.performReading()) {
+    Serial.println("Failed to perform reading :(");
+    yield();
+    return;
+  }
+
+  temperature = bme.temperature + tempOffset;
+  humidity = bme.humidity;
+  pressure = bme.pressure / 100.0f; // hPa
+  gas_resistance = bme.gas_resistance;
+
+  // Print to Serial for debugging
+  printSensorData();
+}
+
+// -------------------- Display --------------------
+void setupDisplay() {
+  tft.initR(INITR_MINI160x80);   // Initialize ST7735
+  tft.fillScreen(ST77XX_BLACK);
+  tft.setRotation(1);          // Adjust rotation if needed
+  tft.invertDisplay(true);
+  //tft.setTextColor(ST77XX_WHITE);
+  //tft.setTextSize(1);
+}
+
+void updateDisplay() {
+  // Determine AQI text
+  String aqiText;
+  if (gas_resistance > 30000) aqiText = "Good";
+  else if (gas_resistance > 15000) aqiText = "Moderate";
+  else aqiText = "Poor";
+
+  // Check if any value changed significantly
+  bool updateRequired = false;
+
+  if (isnan(lastTemperature) || abs(temperature - lastTemperature) >= tempThreshold) updateRequired = true;
+  if (isnan(lastHumidity) || abs(humidity - lastHumidity) >= humidityThreshold) updateRequired = true;
+  if (isnan(lastGas) || abs(gas_resistance - lastGas) >= gasThreshold) updateRequired = true;
+  if (lastAqiText != aqiText) updateRequired = true;
+
+  if (!updateRequired) return; // nothing significant changed
+
+  // Save current values as last displayed
+  lastTemperature = temperature;
+  lastHumidity = humidity;
+  lastGas = gas_resistance;
+  lastAqiText = aqiText;
+
+  // ---- Redraw TFT ----
+  tft.fillScreen(ST77XX_BLACK);  // clear screen
+  int y = 5;
+
+  // Gas
+  tft.setCursor(1, y);
+  tft.setTextColor(ST77XX_BLUE);
+  tft.setTextSize(1);
+  tft.print("Gas: ");
+  tft.println((int)gas_resistance);
+  yield();
+
+  // AQI
+  //tft.setCursor(1, y + 16);
+  tft.setTextColor(ST77XX_GREEN);
+  tft.setTextSize(2);
+  tft.print("AQI: ");
+  tft.println(aqiText);
+  yield();
+
+  // Temperature
+  //tft.setCursor(1, y + 48);
+  tft.setTextColor(ST77XX_RED);
+  tft.setTextSize(2);
+  tft.print("T:   ");
+  tft.println((int)temperature);
+  yield();
+
+  // Humidity
+  //tft.setCursor(1, y + 72);
+  tft.setTextColor(ST77XX_YELLOW);
+  tft.setTextSize(2);
+  tft.print("H:   ");
+  tft.println((int)humidity);
+  yield();
+
+  // Pressure
+  //tft.setCursor(1, y + 96);
+  tft.setTextColor(ST77XX_CYAN);
+  tft.setTextSize(2);
+  tft.print("P:   ");
+  tft.println((int)pressure);
+  yield();
+}
+
+
+
+
+
+
+// -------------------- WiFi + MQTT --------------------
+void reconnectMqtt() {
+  while (!mqttClient.connected()) {
+    Serial.print("Connecting to MQTT...");
+    if (mqttClient.connect("EnviroSensePico", mqtt_user, mqtt_pass)) {
+  publishDiscovery();
+} else {
+      Serial.print("failed, rc=");
+      Serial.print(mqttClient.state());
+      Serial.println(" retrying in 5s");
+      delay(5000);
+    }
+  }
+}
+
+void setupWiFi() {
+  delay(10);
+  Serial.println();
+  Serial.print("Connecting to ");
+  Serial.println(ssid);
+
+  WiFi.begin(ssid, password);
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+  }
+  Serial.println("\nWiFi connected");
+}
+
+// -------------------- Setup --------------------
+void setup() {
+  Serial.begin(115200);
+
+  setupWiFi();
+  mqttClient.setServer(mqtt_server, mqtt_port);
+
+  setupDisplay();
+
+  if (!bme.begin()) {
+    Serial.println("BME680 init failed!");
+    for (;;);
+  }
+
+  // Set up oversampling and filter for faster reading
+  bme.setTemperatureOversampling(BME680_OS_8X);
+  bme.setHumidityOversampling(BME680_OS_2X);
+  bme.setPressureOversampling(BME680_OS_4X);
+  bme.setIIRFilterSize(BME680_FILTER_SIZE_3);
+  bme.setGasHeater(320, 150); // 320°C for 150 ms
+}
+
+// -------------------- Loop --------------------
+void loop() {
+  if (!mqttClient.connected()) {
+    reconnectMqtt();
+  }
+  mqttClient.loop();
+
+  readBME680();
+  updateDisplay();
+
+  if (millis() - lastMqttUpdate >= mqttInterval) {
+    lastMqttUpdate = millis();
+    publishState();
+  }
+}
